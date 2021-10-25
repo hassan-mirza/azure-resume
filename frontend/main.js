bd3998c98d76869c8721dbb7f352e7b2e45b899c
Hassan <HassanFMirza@gmail.com>
2021-10-25 11:48:27 -0500

    touched main.js

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
 });
 
 const functionApi = "https://hmresumecounter.azurewebsites.net/api/GetResumeCounter?code=l8irBj8vfkNaVgzM27xfv4gxFoE2O2WwGFXUJJ9SjtHlc9w739FZGg==";
-//const functionApi = "http://localhost:7071/api/GetResumeCounter";
+//const functionApi = "http://localhost:7071/api/GetResumeCounter";;
 
 const getVisitCount = () => {
     let count = 30;
