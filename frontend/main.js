d77f4def0fbee010d7fc042e8c4a5f325de9ea92
Hassan <HassanFMirza@gmail.com>
2021-10-25 11:36:52 -0500

    added frontend github action

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,8 @@ window.addEventListener("DOMContentLoaded", (e) => {
     getVisitCount();
 });
 
-const functionApi = "http://localhost:7071/api/GetResumeCounter";
+const functionApi = "https://hmresumecounter.azurewebsites.net/api/GetResumeCounter?code=l8irBj8vfkNaVgzM27xfv4gxFoE2O2WwGFXUJJ9SjtHlc9w739FZGg==";
+//const functionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
     let count = 30;
